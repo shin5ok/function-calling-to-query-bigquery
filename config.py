4477019232f116bbbbac734fffe4fac20927b9d3
shin5ok <shin5ok@55mp.com>
2024-09-12 16:13:46 +0900

    just clean up

--- config.py
+++ config.py
@@ -1,14 +1,6 @@
 import os
   
-VERTEX_AI_LOCATION = "global"
 PROJECT_ID = os.environ.get("PROJECT_ID")
-DATASTORE_ID = os.environ.get("DATASTORE_ID")
-
-BUCKET_NAME = os.environ.get("BUCKET_NAME", PROJECT_ID)
-LOCATION = os.environ.get("LOCATION", "europe-west1")
 
 BQ_DATASET_ID = os.environ.get("BQ_DATASET_ID")
 BQ_TABLE_NAME = os.environ.get("BQ_TABLE_NAME")
-
-
-MODEL_VERSION = "gemini-1.5-flash-001/answer_gen/v1"
